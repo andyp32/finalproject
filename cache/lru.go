2ff9d2fc559c889efd85e24550d501faa2f2d507
Andrew Paul <andrew_paul@dynamic-oit-vapornet100-10-8-26-77.princeton.edu>
2022-02-27 14:42:31 -0500

    lru copied from fifo

--- cache/lru.go
+++ cache/lru.go
@@ -8,27 +8,19 @@ type LRU struct {
 	numBindings int
 	// key string, val: {starting point in array, # bytes}
 	location map[string]*Node
-	// storage  *byte
+	front    *Node
+	back     *Node
 	hits     int
 	misses   int
-	root     *Node
-	lru_node *Node
 }
-
-// type Node struct {
-// 	next     *Node
-// 	previous *Node
-// 	key      string
-// 	value    []byte
-// }
 
 // NewLRU returns a pointer to a new LRU with a capacity to store limit bytes
 func NewLru(limit int) *LRU {
 	lru := new(LRU)
+	lru.front = new(Node)
+	lru.back = lru.front
 	lru.limit = limit
 	lru.location = make(map[string]*Node)
-	lru.root = new(Node)
-	lru.lru_node = lru.root
 	lru.hits = 0
 	lru.misses = 0
 	return lru
@@ -44,24 +36,137 @@ func (lru *LRU) RemainingStorage() int {
 	return lru.limit - lru.inUse
 
 }
+func (lru *LRU) PlaceNodeFront(current *Node) bool {
+	if current == lru.front {
+		// Do nothing
+	} else if current == lru.back {
+		// current=back; current -> front; current = front; front -> next
+		lru.back = current.previous
+		lru.back.next = nil
+
+		current.next = lru.front
+		current.previous = nil
+		lru.front = current
+	} else {
+		// current=middle; current -> front; current = front; front -> next
+		current.next.previous = current.previous
+		current.previous.next = current.next
+		current.next = lru.front
+		current.previous = nil
+		lru.front = current
+	}
+	return true
+}
+func (lru *LRU) DeleteNode(current *Node) bool {
+
+	if lru.numBindings == 1 {
+	} else if current == lru.back {
+		// current=back; current -> front; current = front; front -> next
+		lru.back = current.previous
+	} else if current == lru.front {
+		lru.front = current.next
+	} else {
+		// current=middle; current -> front; current = front; front -> next
+		current.next.previous = current.previous
+		current.previous.next = current.next
+	}
+
+	lru.inUse += -len(current.key) - len(lru.location[current.key].value)
+	delete(lru.location, current.key)
+	current = nil
+	lru.numBindings--
+
+	return true
+}
+func (lru *LRU) CreateNode(key string, value []byte) bool {
+	size := len(key) + len(value)
+
+	if lru.numBindings == 0 {
+		lru.front.key = key
+		lru.front.value = value
+		lru.location[key] = lru.front
+
+	} else {
+		new_node := new(Node)
+		new_node.next = lru.front
+		new_node.previous = nil
+		new_node.value = value
+		new_node.key = key
+		new_node.size = size
+		lru.front.previous = new_node
+		lru.front = new_node
+
+		lru.location[key] = new_node
+	}
+
+	lru.inUse += size
+	lru.numBindings++
+	return true
+
+}
 
 // Get returns the value associated with the given key, if it exists.
 // This operation counts as a "use" for that key-value pair
 // ok is true if a value was found and false otherwise.
 func (lru *LRU) Get(key string) (value []byte, ok bool) {
-	return nil, false
+	val, ok := lru.location[key]
+	if ok {
+		lru.PlaceNodeFront(val)
+		lru.hits++
+		return val.value, ok
+
+	} else {
+		lru.misses++
+		return nil, false
+	}
 }
 
 // Remove removes and returns the value associated with the given key, if it exists.
 // ok is true if a value was found and false otherwise
 func (lru *LRU) Remove(key string) (value []byte, ok bool) {
-	return nil, false
+	if val, ok := lru.location[key]; ok {
+		lru.DeleteNode(val)
+		lru.hits++
+		return val.value, ok
+	} else {
+		lru.misses++
+		return nil, false
+	}
 }
 
 // Set associates the given value with the given key, possibly evicting values
 // to make room. Returns true if the binding was added successfully, else false.
 func (lru *LRU) Set(key string, value []byte) bool {
-	return false
+
+	size := len(key) + len(value)
+
+	// if binding exists update old value, if size permits?????
+	//  NOT SURE
+	if val, ok := lru.location[key]; ok {
+		if val.size >= size {
+			lru.DeleteNode(val)
+			lru.CreateNode(key, value)
+			return true
+		}
+		return false
+
+		// else if a existing binding is not found then need to create from scratch
+	} else {
+		if size > lru.limit {
+			return false
+		}
+
+		if lru.RemainingStorage() >= size {
+			lru.CreateNode(key, value)
+			return true
+		} else {
+			for lru.RemainingStorage() < size {
+				lru.DeleteNode(lru.back)
+			}
+			lru.CreateNode(key, value)
+			return true
+		}
+	}
 }
 
 // Len returns the number of bindings in the LRU.
